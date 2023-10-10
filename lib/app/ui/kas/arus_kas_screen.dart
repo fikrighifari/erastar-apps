226e2a4925033dd71d53a00a4f313a3ba0304ef3
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-10-10 09:41:55 +0700

    add: tabview in arus kas screen

--- lib/app/ui/kas/arus_kas_screen.dart
+++ lib/app/ui/kas/arus_kas_screen.dart
@@ -20,92 +20,132 @@ class _ArusKasScreenState extends State<ArusKasScreen>
   }
 
   @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      hideAppBar: true,
-      hideBackButton: true,
-      child: Column(
-        children: [
-          InkWell(
-            onTap: () {
-              Modular.to.pushNamed('/profile/');
-            },
-            child: CustomContainer(
-              padding: EdgeInsets.symmetric(
-                horizontal: defaultMargin,
-                vertical: 10,
-              ),
-              width: double.infinity,
-              backgroundColor: AppColor.primayRedColor,
-              child: Row(
-                children: [
-                  // avatarProfile != null
-                  //     ? SizedBox(
-                  //         width: 50,
-                  //         height: 50,
-                  //         child: CircleAvatar(
-                  //           backgroundImage: NetworkImage(
-                  //             '$baseAPIUrlImage$avatarProfile',
-                  //           ),
-                  //           backgroundColor: AppColor.whiteColor,
-                  //         ),
-                  //       )
-                  //     :
-                  SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: SvgPicture.asset(
-                      'assets/icons/ic_default_avatar.svg',
-                      colorFilter: const ColorFilter.mode(
-                        AppColor.whiteColor,
-                        BlendMode.srcIn,
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                Modular.to.pushNamed('/profile/');
+              },
+              child: CustomContainer(
+                padding: EdgeInsets.symmetric(
+                  horizontal: defaultMargin,
+                  vertical: 10,
+                ),
+                width: double.infinity,
+                backgroundColor: AppColor.primayRedColor,
+                child: Row(
+                  children: [
+                    // avatarProfile != null
+                    //     ? SizedBox(
+                    //         width: 50,
+                    //         height: 50,
+                    //         child: CircleAvatar(
+                    //           backgroundImage: NetworkImage(
+                    //             '$baseAPIUrlImage$avatarProfile',
+                    //           ),
+                    //           backgroundColor: AppColor.whiteColor,
+                    //         ),
+                    //       )
+                    //     :
+                    SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: SvgPicture.asset(
+                        'assets/icons/ic_default_avatar.svg',
+                        colorFilter: const ColorFilter.mode(
+                          AppColor.whiteColor,
+                          BlendMode.srcIn,
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Column(
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextWidget(
+                          'nameProfile',
+                          color: AppColor.whiteColor,
+                        ),
+                        TextWidget(
+                          'roleProfile',
+                          color: AppColor.whiteColor,
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    SvgPicture.asset(
+                      'assets/icons/ic_notification.svg',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(defaultMargin),
+                  child: TabBar(
+                      labelColor: Colors.black,
+                      unselectedLabelColor: Colors.grey,
+                      indicator: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5)),
+                      controller: tabController,
+                      tabs: const [
+                        Tab(
+                          text: "Arus Kas",
+                        ),
+                        Tab(
+                          text: "Menunggu Persetujuan",
+                        ),
+                      ]),
+                ),
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                controller: tabController,
+                children: const [
+                  Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      TextWidget(
-                        'nameProfile',
-                        color: AppColor.whiteColor,
-                      ),
-                      TextWidget(
-                        'roleProfile',
-                        color: AppColor.whiteColor,
-                      ),
+                      // _filterKas(),
+                      // _contentKas(),
+                      Text('Arus Kas')
                     ],
                   ),
-                  const Spacer(),
-                  SvgPicture.asset(
-                    'assets/icons/ic_notification.svg',
-                  ),
+                  Column(
+                    children: [
+                      Text('Approval')
+                      // ApprovalCard(
+                      //   approvalModel: ApprovalModel(
+                      //     title: "Sewa Pengacara",
+                      //     description:
+                      //         "Biaya sewa pengacara untuk sengketa rumah lelang dengan ID Listing M000035",
+                      //     dateTime: DateTime.now(),
+                      //     price: 50000000,
+                      //   ),
+                      // ),
+                    ],
+                  )
                 ],
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(defaultMargin),
-            child: TabBar(
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey,
-                indicator: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5)),
-                controller: tabController,
-                tabs: const [
-                  Tab(
-                    text: "Arus Kas",
-                  ),
-                  Tab(
-                    text: "Menunggu Persetujuan",
-                  ),
-                ]),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
