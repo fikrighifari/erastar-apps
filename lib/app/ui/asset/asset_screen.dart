e9a4f36246f87fb015b717368041e8940caf57e7
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-10-12 16:47:27 +0700

    update: asset screen using future builder

--- lib/app/ui/asset/asset_screen.dart
+++ lib/app/ui/asset/asset_screen.dart
@@ -24,6 +24,22 @@ class _AssetScreenState extends State<AssetScreen> {
   late DataProfile dataProfile;
   late String? nameProfile, roleProfile, avatarProfile = '';
 
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> refreshAsset() async {
+    setState(() {
+      _refreshIndicatorKey.currentState!.show();
+      fetchData();
+    });
+  }
+
   fetchData() async {
     //Get Profile
     futureProfile = AssetController().getProfile();
@@ -54,165 +70,216 @@ class _AssetScreenState extends State<AssetScreen> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      hideBackButton: true,
-      hideAppBar: true,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Header Content
-          InkWell(
-            onTap: () {
-              Modular.to.pushNamed('/profile/');
-            },
-            child: CustomContainer(
-              padding: EdgeInsets.symmetric(
-                horizontal: defaultMargin,
-                vertical: 10,
-              ),
-              width: double.infinity,
-              backgroundColor: AppColor.primayRedColor,
-              child: Row(
-                children: [
-                  // avatarProfile != null
-                  //     ? SizedBox(
-                  //         width: 50,
-                  //         height: 50,
-                  //         child: CircleAvatar(
-                  //           backgroundImage: NetworkImage(
-                  //             '$baseAPIUrlImage$avatarProfile',
-                  //           ),
-                  //           backgroundColor: AppColor.whiteColor,
-                  //         ),
-                  //       )
-                  //     :
-                  SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: SvgPicture.asset(
-                      'assets/icons/ic_default_avatar.svg',
-                      colorFilter: const ColorFilter.mode(
-                        AppColor.whiteColor,
-                        BlendMode.srcIn,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  const Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextWidget(
-                        'nameProfile',
-                        color: AppColor.whiteColor,
-                      ),
-                      TextWidget(
-                        'roleProfile',
-                        color: AppColor.whiteColor,
-                      ),
-                    ],
-                  ),
-                  const Spacer(),
-                  SvgPicture.asset(
-                    'assets/icons/ic_notification.svg',
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: EdgeInsets.all(defaultMargin),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextWidget.bodyLarge(
-                  'Listing Properti',
-                  fontWeight: boldWeight,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      child: CustomTextField(
-                        prefixIcon: const Icon(Icons.search),
-                        hintText: "Cari Properti",
-                        textStyle: defaultTextStyle,
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // _showBottomSheetFilter(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(70, 40),
-                        backgroundColor: AppColor.whiteColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                          side: const BorderSide(
-                            color: AppColor.blackColor,
+    return Scaffold(
+      body: FutureBuilder<AssetModel?>(
+          future: futureAsset,
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Text('Press button to start');
+              case ConnectionState.active:
+                return const Text('Press button to start.');
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.done:
+                return isLoading
+                    ? SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: const Center(
+                          child: CircularProgressIndicator(
+                            color: AppColor.primayRedColor,
                           ),
                         ),
-                      ),
-                      child: const TextWidget(
-                        "+ Filter",
-                      ),
-                    ),
-                  ],
-                ),
-                // AssetCard()
-                Container(
-                    margin: EdgeInsets.only(top: defaultMargin),
-                    // color: Colors.red,
-                    child: listAsset!.isNotEmpty
-                        ? GridView.count(
-                            primary: true,
-                            crossAxisCount: 2,
-                            shrinkWrap: true,
-                            scrollDirection: Axis.vertical,
-                            physics: const NeverScrollableScrollPhysics(),
-                            childAspectRatio:
-                                MediaQuery.of(context).size.width /
-                                    (MediaQuery.of(context).size.height / 1.18),
-                            // mainAxisSpacing: 5.0,
-                            crossAxisSpacing: 5.0,
-                            children: listAsset!.map((dt) {
-                              return Column(
-                                children: [
-                                  AssetCard(
-                                    assetCardModel: AssetCardModel(
-                                        title: dt.title,
-                                        idListing: dt.listingId,
-                                        address: dt.address,
-                                        type: dt.type,
-                                        price: dt.price,
-                                        date: dt.createdAt.toString(),
-                                        imgUrl: baseAPIUrlImage +
-                                            dt.images!.path.toString() +
-                                            dt.images!.filename.toString()),
+                      )
+                    : RefreshIndicator(
+                        onRefresh: refreshAsset,
+                        child: SafeArea(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Header Content
+                                InkWell(
+                                  onTap: () {
+                                    Modular.to.pushNamed('/profile/');
+                                  },
+                                  child: CustomContainer(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: defaultMargin,
+                                      vertical: 10,
+                                    ),
+                                    width: double.infinity,
+                                    backgroundColor: AppColor.primayRedColor,
+                                    child: Row(
+                                      children: [
+                                        // avatarProfile != null
+                                        //     ? SizedBox(
+                                        //         width: 50,
+                                        //         height: 50,
+                                        //         child: CircleAvatar(
+                                        //           backgroundImage: NetworkImage(
+                                        //             '$baseAPIUrlImage$avatarProfile',
+                                        //           ),
+                                        //           backgroundColor: AppColor.whiteColor,
+                                        //         ),
+                                        //       )
+                                        //     :
+                                        SizedBox(
+                                          width: 50,
+                                          height: 50,
+                                          child: SvgPicture.asset(
+                                            'assets/icons/ic_default_avatar.svg',
+                                            colorFilter: const ColorFilter.mode(
+                                              AppColor.whiteColor,
+                                              BlendMode.srcIn,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(
+                                          width: 10,
+                                        ),
+                                        const Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            TextWidget(
+                                              'nameProfile',
+                                              color: AppColor.whiteColor,
+                                            ),
+                                            TextWidget(
+                                              'roleProfile',
+                                              color: AppColor.whiteColor,
+                                            ),
+                                          ],
+                                        ),
+                                        const Spacer(),
+                                        SvgPicture.asset(
+                                          'assets/icons/ic_notification.svg',
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ],
-                              );
-                            }).toList(),
-                          )
-                        : Text('Data Asset Kosong')),
-              ],
-            ),
-          )
-        ],
-      ),
+                                ),
+
+                                Padding(
+                                  padding: EdgeInsets.all(defaultMargin),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      TextWidget.bodyLarge(
+                                        'Listing Properti',
+                                        fontWeight: boldWeight,
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          SizedBox(
+                                            width: MediaQuery.of(context)
+                                                    .size
+                                                    .width /
+                                                1.5,
+                                            child: CustomTextField(
+                                              prefixIcon:
+                                                  const Icon(Icons.search),
+                                              hintText: "Cari Properti",
+                                              textStyle: defaultTextStyle,
+                                            ),
+                                          ),
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              // _showBottomSheetFilter(context);
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                                              fixedSize: const Size(70, 40),
+                                              backgroundColor:
+                                                  AppColor.whiteColor,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(10),
+                                                side: const BorderSide(
+                                                  color: AppColor.blackColor,
+                                                ),
+                                              ),
+                                            ),
+                                            child: const TextWidget(
+                                              "+ Filter",
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      // AssetCard()
+                                      Container(
+                                          margin: EdgeInsets.only(
+                                              top: defaultMargin),
+                                          // color: Colors.red,
+                                          child: listAsset!.isNotEmpty
+                                              ? GridView.count(
+                                                  primary: true,
+                                                  crossAxisCount: 2,
+                                                  shrinkWrap: true,
+                                                  scrollDirection:
+                                                      Axis.vertical,
+                                                  physics:
+                                                      const NeverScrollableScrollPhysics(),
+                                                  childAspectRatio:
+                                                      MediaQuery.of(context)
+                                                              .size
+                                                              .width /
+                                                          (MediaQuery.of(
+                                                                      context)
+                                                                  .size
+                                                                  .height /
+                                                              1.18),
+                                                  // mainAxisSpacing: 5.0,
+                                                  crossAxisSpacing: 5.0,
+                                                  children:
+                                                      listAsset!.map((dt) {
+                                                    return Column(
+                                                      children: [
+                                                        AssetCard(
+                                                          assetCardModel: AssetCardModel(
+                                                              title: dt.title,
+                                                              idListing:
+                                                                  dt.listingId,
+                                                              address:
+                                                                  dt.address,
+                                                              type: dt.type,
+                                                              price: dt.price,
+                                                              date: dt.createdAt
+                                                                  .toString(),
+                                                              imgUrl: baseAPIUrlImage +
+                                                                  dt.images!
+                                                                      .path
+                                                                      .toString() +
+                                                                  dt.images!
+                                                                      .filename
+                                                                      .toString()),
+                                                        ),
+                                                      ],
+                                                    );
+                                                  }).toList(),
+                                                )
+                                              : Text('Data Asset Kosong')),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+            }
+          }),
     );
   }
 }
