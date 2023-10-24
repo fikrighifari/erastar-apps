ab9f7e3a783648f54547b22dece9d8214fcbdaea
Fikri Akbar Ghifari <vinalesnow@gmail.com>
2023-10-24 14:23:27 +0700

    update top marketing screen

--- lib/app/ui/sales/top_marketing_screen.dart
+++ lib/app/ui/sales/top_marketing_screen.dart
@@ -16,6 +16,8 @@ class _TopMarketingScreenState extends State<TopMarketingScreen> {
   bool isLoading = false;
   late Future<TopMarketingModel?> futureTopMarketing;
   late List<DataListTopMarketing>? listTopMarketing = [];
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
 
   String? month = '';
   String? topName = '';
@@ -26,6 +28,13 @@ class _TopMarketingScreenState extends State<TopMarketingScreen> {
   void initState() {
     super.initState();
     fetchData();
+  }
+
+  Future<void> refreshTopMarketing() async {
+    setState(() {
+      _refreshIndicatorKey.currentState!.show();
+      fetchData();
+    });
   }
 
   fetchData() async {
@@ -52,147 +61,205 @@ class _TopMarketingScreenState extends State<TopMarketingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
-      hideAppBar: true,
-      hideBackButton: true,
-      backgroundColor: AppColor.whiteColor,
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(defaultMargin),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const TextWidget.titleMedium('Top Sales Marketing'),
-                  CustomContainer(
-                    margin: const EdgeInsets.only(top: 8),
-                    backgroundColor: AppColor.primayRedColor,
-                    padding: EdgeInsets.all(defaultMargin),
-                    radius: 4,
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Image.asset(
-                          "assets/images/img_trophy.png",
-                          width: 50,
-                          height: 50,
+    return Scaffold(
+      body: FutureBuilder(
+          future: futureTopMarketing,
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Text('Press button to start');
+              case ConnectionState.active:
+                return const Text('Press button to start.');
+              case ConnectionState.waiting:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              case ConnectionState.done:
+                return isLoading
+                    ? SizedBox(
+                        height: MediaQuery.of(context).size.height,
+                        width: MediaQuery.of(context).size.width,
+                        child: const Center(
+                          child: CircularProgressIndicator(
+                            color: AppColor.primayRedColor,
+                          ),
                         ),
-                        const SizedBox(width: 8),
-                        Expanded(
+                      )
+                    : RefreshIndicator(
+                        onRefresh: refreshTopMarketing,
+                        key: _refreshIndicatorKey,
+                        child: SafeArea(
+                            child: SingleChildScrollView(
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Container(
-                                padding: const EdgeInsets.only(bottom: 0.0),
+                              Padding(
+                                padding: EdgeInsets.all(defaultMargin),
                                 child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      const TextWidget.titleSmall(
-                                        'Top Of The Month',
-                                        color: AppColor.whiteColor,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const TextWidget.titleMedium(
+                                        'Top Sales Marketing'),
+                                    CustomContainer(
+                                      margin: const EdgeInsets.only(top: 8),
+                                      backgroundColor: AppColor.primayRedColor,
+                                      padding: EdgeInsets.all(defaultMargin),
+                                      radius: 4,
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Image.asset(
+                                            "assets/images/img_trophy.png",
+                                            width: 50,
+                                            height: 50,
+                                          ),
+                                          const SizedBox(width: 8),
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Container(
+                                                  padding:
+                                                      const EdgeInsets.only(
+                                                          bottom: 0.0),
+                                                  child: Column(
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        const TextWidget
+                                                            .titleSmall(
+                                                          'Top Of The Month',
+                                                          color: AppColor
+                                                              .whiteColor,
+                                                        ),
+                                                        TextWidget.titleSmall(
+                                                          month.toString(),
+                                                          color: AppColor
+                                                              .whiteColor,
+                                                        ),
+                                                        const TextWidget
+                                                            .titleSmall(
+                                                          'Marketing Associate',
+                                                          color: AppColor
+                                                              .whiteColor,
+                                                        ),
+                                                      ]),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          // avatarProfile!.isNotEmpty
+                                          //     ? Image.network(
+                                          //         '$baseAPIUrlImage$avatarProfile',
+                                          //         width: 50,
+                                          //       )
+                                          //     :
+                                          Image.asset(
+                                            "assets/images/img_trophy.png",
+                                            width: 50,
+                                            height: 50,
+                                          ),
+                                          const SizedBox(
+                                            width: 5,
+                                          ),
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              Column(
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    const TextWidget.titleSmall(
+                                                      'National',
+                                                      color:
+                                                          AppColor.whiteColor,
+                                                    ),
+                                                    const TextWidget.titleSmall(
+                                                      'ERA STAR',
+                                                      color:
+                                                          AppColor.whiteColor,
+                                                    ),
+                                                    TextWidget.titleSmall(
+                                                      topName.toString(),
+                                                      color:
+                                                          AppColor.whiteColor,
+                                                    ),
+                                                  ]),
+                                            ],
+                                          ),
+                                        ],
                                       ),
-                                      TextWidget.titleSmall(
-                                        month.toString(),
-                                        color: AppColor.whiteColor,
-                                      ),
-                                      const TextWidget.titleSmall(
-                                        'Marketing Associate',
-                                        color: AppColor.whiteColor,
-                                      ),
-                                    ]),
-                              ),
+                                    ),
+                                    Container(
+                                      margin:
+                                          EdgeInsets.only(top: defaultMargin),
+                                      // color: Colors.red,
+                                      child: listTopMarketing!.isNotEmpty
+                                          ? GridView.count(
+                                              primary: true,
+                                              crossAxisCount: 2,
+                                              shrinkWrap: true,
+                                              scrollDirection: Axis.vertical,
+                                              physics:
+                                                  const NeverScrollableScrollPhysics(),
+                                              childAspectRatio:
+                                                  MediaQuery.of(context)
+                                                          .size
+                                                          .width /
+                                                      (MediaQuery.of(context)
+                                                              .size
+                                                              .height /
+                                                          1.18),
+                                              // mainAxisSpacing: 5.0,
+                                              crossAxisSpacing: 5.0,
+                                              children:
+                                                  listTopMarketing!.map((dt) {
+                                                return Column(
+                                                  children: [
+                                                    TopMarketingCard(
+                                                        topMarketingCardModel:
+                                                            TopMarketingCardModel(
+                                                      avatarUrl:
+                                                          baseAPIUrlImage +
+                                                              dt.avatarPath! +
+                                                              dt.avatar
+                                                                  .toString(),
+                                                      provinceName: dt
+                                                          .provinsi!.lokasiNama,
+                                                      officeName:
+                                                          dt.office!.name,
+                                                      marketingName: dt.name,
+                                                    ))
+                                                  ],
+                                                );
+                                              }).toList(),
+                                            )
+                                          : const EmptyData(
+                                              text:
+                                                  'Data Top Marketing tidak ditemukan',
+                                            ),
+                                    ),
+                                  ],
+                                ),
+                              )
                             ],
                           ),
-                        ),
-                        // avatarProfile!.isNotEmpty
-                        //     ? Image.network(
-                        //         '$baseAPIUrlImage$avatarProfile',
-                        //         width: 50,
-                        //       )
-                        //     :
-                        Image.asset(
-                          "assets/images/img_trophy.png",
-                          width: 50,
-                          height: 50,
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const TextWidget.titleSmall(
-                                    'National',
-                                    color: AppColor.whiteColor,
-                                  ),
-                                  const TextWidget.titleSmall(
-                                    'ERA STAR',
-                                    color: AppColor.whiteColor,
-                                  ),
-                                  TextWidget.titleSmall(
-                                    topName.toString(),
-                                    color: AppColor.whiteColor,
-                                  ),
-                                ]),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: defaultMargin),
-                    // color: Colors.red,
-                    child: listTopMarketing!.isNotEmpty
-                        ? GridView.count(
-                            primary: true,
-                            crossAxisCount: 2,
-                            shrinkWrap: true,
-                            scrollDirection: Axis.vertical,
-                            physics: const NeverScrollableScrollPhysics(),
-                            childAspectRatio:
-                                MediaQuery.of(context).size.width /
-                                    (MediaQuery.of(context).size.height / 1.18),
-                            // mainAxisSpacing: 5.0,
-                            crossAxisSpacing: 5.0,
-                            children: listTopMarketing!.map((dt) {
-                              return Column(
-                                children: [
-                                  TopMarketingCard(
-                                      topMarketingCardModel:
-                                          TopMarketingCardModel(
-                                    avatarUrl: baseAPIUrlImage +
-                                        dt.avatarPath! +
-                                        dt.avatar.toString(),
-                                    provinceName: dt.provinsi!.lokasiNama,
-                                    officeName: dt.office!.name,
-                                    marketingName: dt.name,
-                                  ))
-                                ],
-                              );
-                            }).toList(),
-                          )
-                        : const EmptyData(
-                            text: 'Data Top Marketing tidak ditemukan',
-                          ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+                        )),
+                      );
+            }
+          }),
     );
   }
 }
